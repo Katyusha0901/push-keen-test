eee4d0fb759a59117331fc76d35a65bcdd3280af
Ekaterina Bondareva <1978231@bk.ru>
2024-04-09 22:23:22 +0500

    creating card structure

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,3 +1,18 @@
-export function Card(){
-    return <div className="card"></div>
-} 
+export function Card() {
+  return (
+    <div className="card">
+      <div className="card__context">
+        <div className="card__title">Lorem Ipsum dolor sit</div>
+        <div className="card__text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+      </div>
+    </div>
+  );
+}
